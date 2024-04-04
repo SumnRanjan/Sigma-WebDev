2a18c8428bcb4b335d824b6901b987c94363605d
Suman <sumanranjan17291729@gmail.com>
2024-04-05 01:09:49 +0530

    loop

--- SIGMA-0-JS/3Loop.js
+++ SIGMA-0-JS/3Loop.js
@@ -1,4 +1,4 @@
-console.log("Hey i am a LOOp")
+// console.log("Hey i am a LOOp");
 
 // For looop ki bateee.......
 
@@ -27,7 +27,6 @@ console.log("Hey i am a LOOp")
 //     console.log(c)
 // }
 
-
 // While ki bateee Darling.....
 
 // let i = 1;
@@ -36,8 +35,67 @@ console.log("Hey i am a LOOp")
 //     i++;
 // }
 
-let i = 10;
-do {
-    console.log(i)
-    i++;
-} while (i<6);
+// let i = 10;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i<6);
+
+// For loop --
+
+// const love = [1 ,4 ,3]
+// for(let i = 0 ; i < love.length ; i++){
+//     console.log( i , love[i])
+// }
+
+// Nested for loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`i is : ${i}`)
+//     for(let j = 1 ; j <=3 ; j++){
+//         console.log(`    j  is : ${j}`)
+//     }
+// }
+
+// const l = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+
+// for(let  i = 0 ; i < l.length ; i++){
+//     const row = l[i]
+//     for(let j = 0 ; j < row.length ; j++){
+//         console.log(row[j])
+//     }
+// }
+
+
+//while loop 
+
+// let l = 0
+// while( l <= 10){
+//     l++;
+//     console.log(l)
+// }
+
+
+// const s = "sim"
+// let guess = prompt("enter")
+
+// while (guess !== s){
+//     guess = prompt("enter")
+// }
+// console.log('congo')
+
+
+//break
+
+// let s = prompt("enter something")
+// while (true){
+//     s = prompt(s)
+//     if(s.toLowerCase() === "stop") break;
+
+// }
+// console.log("You Win")
