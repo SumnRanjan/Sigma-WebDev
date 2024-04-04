7a7a4213ccc4f06643d6ac956181cad5d4e27923
Suman <sumanranjan17291729@gmail.com>
2024-04-04 08:34:31 +0530

    obj

--- SIGMA-0-JS/10Objects.js
+++ SIGMA-0-JS/10Objects.js
@@ -50,19 +50,63 @@
 
 //Cw --
 
-let Book = {
-    Title : "Wings Of Fire",
-    Author : {
-        name : "APJ Abdul Kalam",
-        age : 65
-    }
-}
+// let Book = {
+//     Title : "Wings Of Fire",
+//     Author : {
+//         name : "APJ Abdul Kalam",
+//         age : 65
+//     }
+// }
 
-console.log(Book.Title)
-console.log(Book.Author.name)
+// console.log(Book.Title)
+// console.log(Book.Author.name)
 
-Book.Title = "New Title"
-Book.Author.name = "New Name"
+// Book.Title = "New Title"
+// Book.Author.name = "New Name"
 
-console.log(Book.Title)
-console.log(Book.Author.name)
+// console.log(Book.Title)
+// console.log(Book.Author.name)
+
+
+
+
+// --------------------------------------------------------
+
+// let commet = {
+//     username : 'sumn',
+//     downvotes : 19 ,
+//     isFunny :true,
+//     color : ['blue' , 'red']
+// }
+
+// // console.log(commet["username"]) //we can also acces data form object
+
+// console.log(commet.isFunny);
+
+// const years = {1 : "good" , 2 : "bad"}
+// console.log(years[1]);
+// let su ;
+
+// console.log(years[su])
+
+
+// const dumdub = {true : 'dat' , null : "asdjp"}
+// console.log(dumdub.true)
+
+//update object --
+// const midterms = {suman : 96 , suanina : 78}
+// midterms.suanina = 79
+// midterms.suman = 'c++'
+// midterms['suanina'] = 'A'
+// midterms.sumit = 'B+'
+// midterms['shulab'] = 'A+'
+// console.log(midterms);
+
+// arry and object 
+
+const comment = [
+    {username : 'suman' , text : 'lolololo' , votes: 9},
+    {username: 'sunaina', text :'glub glub' , votes: 123889}
+]
+
+console.log(comment[1].text)
